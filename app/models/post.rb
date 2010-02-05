178bb06839bb44978f42c922c9348bfe783da8b1
Jeremy McAnally <jeremymcanally@gmail.com>
2010-02-04 23:36:15 -0600

    Add validations on Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   before_save :render_body
   
+  validates :title, :presence => true
+  validates :body, :presence => true
+  
   def render_body
     self.rendered_body = RDiscount.new(self.body).to_html
   end
