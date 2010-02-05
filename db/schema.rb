e9320815f52ece359cfe4cefb95100b3a4f6e561
Jeremy McAnally <jeremymcanally@gmail.com>
2010-02-04 23:42:33 -0600

    Add new schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100205042616) do
+ActiveRecord::Schema.define(:version => 20100205050927) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "rendered_body"
   end
 
 end
