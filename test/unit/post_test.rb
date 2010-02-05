930e8868b0e4d8904d6f5090f6b445b0c428f71f
Jeremy McAnally <jeremymcanally@gmail.com>
2010-02-04 23:36:06 -0600

    Tests for validations on Post

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -6,4 +6,16 @@ class PostTest < ActiveSupport::TestCase
     
     assert_equal "<p>Now <em>this</em> is an awesome post.</p>", post.rendered_body.chomp
   end
+  
+  test "requires title" do
+    post = Post.create(:body => "Now *this* is an awesome post.")
+    assert !post.valid?
+    assert post.errors[:title]
+  end
+  
+  test "requires body" do
+    post = Post.create(:title => "This post rocks.")
+    assert !post.valid?
+    assert post.errors[:body]
+  end
 end
