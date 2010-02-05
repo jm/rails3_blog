af83a5a2e85a1679896e989f6828d1f5ee4aa7d3
Jeremy McAnally <jeremymcanally@gmail.com>
2010-02-04 23:22:08 -0600

    Test for rendered body

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "renders Markdown after save" do
+    post = Post.create(:title => "This post rocks.", :body => "Now *this* is an awesome post.")
+    
+    assert_equal "<p>Now <em>this</em> is an awesome post.</p>", post.rendered_body.chomp
   end
 end
